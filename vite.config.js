b0345dcf766a2510e8d96394e8e503f8513725b9
imsea2 <imseaofsea@gmail.com>
2025-08-21 15:42:59 +0900

    port

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-      base: '/my-portfoilo/', // 여기에 실제 저장소 이름 입력
-  build: {
-    outDir: 'dist'
-  }
-})
+  base: '/my-portfolio/', // ★ gh-pages라면 리포지토리 이름으로 수정
+});
